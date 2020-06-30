99c205613083ab46902944848e775868cb4b1877
Lakshman Veti <lakshman.veti@gmail.com>
2020-06-30 22:03:38 +0530

    timezones added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import navList from '../../data/navigation.json';
+import Clock from 'react-live-clock'
+import navList from '../../data/navigation.json'
+
 class Navbar extends React.Component {
   constructor() {
     super()
     this.state = {
       left: 0,
-      width: 0
+      width: 0,
+      selectedCityTimeZone: ''
     }
   }
   handleMouseOut = (e) => {
     e.target.className="link";
     this.setState({
-      isHovered: !this.state.isHovered
+      isHovered: !this.state.isHovered,
+      selectedCityTimeZone: ''
     });
     
   }
 
-  handleMouseEnter = (e) => {
+  handleMouseEnter = (e, item) => {
     e.target.className+=" active";
+    console.log(item.timezone)
     this.setState({
       left: e.target.getBoundingClientRect().x - 8,
-      width: e.target.offsetWidth
+      width: e.target.offsetWidth,
+      selectedCityTimeZone: item.timezone
     });
    
   }
@@ -30,11 +36,15 @@ class Navbar extends React.Component {
         <ul>
         {navList.cities.map(el => {
             return (
-              <li><a href="#" className='link' onMouseLeave={this.handleMouseOut} onMouseEnter={this.handleMouseEnter}>{el.label}</a></li>
+              <li><a href="#" className='link' onMouseLeave={this.handleMouseOut} onMouseEnter={(evt) => this.handleMouseEnter(evt, el)}>{el.label}</a></li>
             );
           })}
         </ul>  
         <div className="line" style={{ left: this.state.left, width: this.state.width }}/>
+        {this.state.selectedCityTimeZone!='' &&
+          <Clock format={'HH:mm:ss'} timezone={this.state.selectedCityTimeZone} />
+        }
+        
       </nav>
     );
   }
